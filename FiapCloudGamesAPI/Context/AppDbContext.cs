a0edb7e27134528d8dd028893d466a4a47d47c58
Gabriel Paulino Farias da Silva <gabrieltrilogia@gmail.com>
2025-05-06 00:07:51 -0300

    Iniciando os projetos FiapCloudGames

---
+++ FiapCloudGamesAPI/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FiapCloudGamesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FiapCloudGamesAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>()
+                .HasKey(u => u.UserId);
+        }
+    }
+}
---
+++ FiapCloudGamesTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace FiapCloudGamesTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
