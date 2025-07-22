4ae51d2faa5c00eb782942371be03915d71c9512
Gabriel Paulino Farias da Silva <gabrieltrilogia@gmail.com>
2025-07-22 00:34:34 -0300

    EnableRetryOnFailure

--- FiapCloudGamesAPI/Program.cs
+++ FiapCloudGamesAPI/Program.cs
@@ -48,7 +48,7 @@ builder.Services.AddAuthorization(options =>
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlOptions => sqlOptions.EnableRetryOnFailure()));
 
 builder.Services.Configure<ConfigSecret>(builder.Configuration.GetSection("ConfigSecret"));
 
