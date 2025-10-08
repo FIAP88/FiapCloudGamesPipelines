d085ef66de5ab99fe7823af2c665327358920e74
LeonardoPerles <111097914+LeonardoPerles@users.noreply.github.com>
2025-10-08 14:45:31 -0300

    Criando Entidade do EventStore para Eventos

---
+++ FCG_Usuarios/src/fiapcloudgames.usuario.Infrastructure/EventStore/StoredEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace fiapcloudgames.usuario.Infrastructure.EventStore
+{ 
+	public class StoredEvent
+	{
+		public Guid Id { get; set; }
+		public string AggregateId { get; set; }
+		public int Version { get; set; }
+		public DateTime Timestamp { get; set; }
+		public string EventType { get; set; }
+		public string Payload { get; set; }
+	}	
+}
